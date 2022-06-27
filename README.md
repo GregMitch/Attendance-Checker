dc4fb1e1a80836be34d9ab0e1076dbb3718bca8b
Gregory Mitchell <grmitch1@gmail.com>
2022-06-27 12:17:32 -0400

    Moving CSV parser to GitHub

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Attendance-Checker
-Simple python script that parses through all csv files in the current directory counting the times names appear.
+
+Simple python script that parses through all csv files in the current directory counting the times names appear. Simply need to place all csv files into the same folder as the TimeSaver.py script.
+
+## Example CSV file
+
+Order number,Ticket number,First Name,Last Name,Email,Twitter,Company,Title,Featured,Ticket type,Discount,Price,Currency,Number of tickets,Paid by (name),Paid by (email),Paid date (UTC),Checkin Date (UTC)
+DSCE99999999,DSCA99999999,Firstname,Lastname,name@email.com,,,,,RSVP,,0.00,USD,1,Firstname Lastname,name@email.com,2020-04-05 23:35:42+00:00,2020-04-06 22:00:05+00:00
---
+++ TimeSaver.py
@@ -0,0 +1,23 @@
+import csv
+import os
+import operator
+rootdir = './' #Current directory
+
+attendance_dict = {"Paid by (name)" : 0}
+for file in os.listdir(rootdir):
+    if file != "TimeSaver.py" and file != "README.md" and file != ".git":
+        with open(file, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                if line[14] in attendance_dict:
+                    if line[17] != "":
+                        attendance_dict[line[14]] += 1
+                else:
+                    attendance_dict[line[14]] = 1
+    else:
+        exit
+
+sorted_dict = dict(sorted(attendance_dict.items(), key=operator.itemgetter(1), reverse=True))
+
+for x, y in sorted_dict.items():
+    print(x, y)
